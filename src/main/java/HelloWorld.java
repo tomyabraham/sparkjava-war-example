d8780ec68b7940c2cdb7e684dfabf8c1a38a242e
tomyabraham <97011123+tomyabraham@users.noreply.github.com>
2022-01-09 22:01:23 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello Tomy New");
+		get("/hello", (req, res) -> "Hello Tomy New@22.01");
 	}
 }
